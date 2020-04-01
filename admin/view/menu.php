ec564f7aecbed282225c985e01460565c9c563f4
Yohan <yohan.g99@gmail.com>
2020-04-01 20:33:24 +0200

    Menu

--- admin/view/menu.php
+++ admin/view/menu.php
@@ -1,12 +1,39 @@
-<a class="nav-link" href="/">RETOUR</a>
-<a class="nav-link" href="/admin/account">Liste des admins</a>
-<a class="nav-link" href="/admin/prestation">Liste des prestations</a>
-<a onClick="destroy()" href="/">DECONNEXION</a>
+<a class="nav-header" href="/admin">
+<img src="/include/images/logo.svg" alt="Explore logo" width="150px" height="150px"/>
+</a>
+
+<a class="nav-item" href="/admin/account">
+  <span>Liste des admins</span>
+  <div class="icon">
+  <i class="fas fa-user-cog"></i>
+  </div>
+</a>
+
+<a class="nav-item" href="/admin/prestation">
+  <span>Liste des prestations</span>
+  <div class="icon">
+  <i class="fas fa-hands-helping"></i>
+  </div>
+</a>
+
+<a class='nav-item' onClick="destroy()" href="/">
+<span>Deconnexion</span>
+  <div class="icon">
+    <i class="fas fa-sign-out-alt"></i>
+  </div>
+</a>
+
+<a class="nav-item" href="/">
+<span>Retour sur le site</span>
+  <div class="icon">
+    <i class="fas fa-arrow-left"></i>
+  </div>
+</a>
 
 
 
 <script>
-function destroy(){
+  function destroy() {
     document.cookie = "PHPSESSID=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-}
+  }
 </script>
